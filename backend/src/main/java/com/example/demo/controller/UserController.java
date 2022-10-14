f50bf90f48f695c106664fabd24854b4e6d3eb93
kelvin-had1 <kelvinmendes900@gmail.com>
2022-10-14 19:13:31 -0300

    Create User

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -3,7 +3,10 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,7 @@ import com.example.demo.services.UserService;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    
+
     @Autowired
     UserService userService;
 
@@ -23,6 +26,11 @@ public class UserController {
     public List<User> getUsers(){
         
         return userService.getUsers();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody User user){
+        return userService.createUser(user);
     }
 
 
--- backend/src/main/java/com/example/demo/services/UserService.java
+++ backend/src/main/java/com/example/demo/services/UserService.java
@@ -1,8 +1,10 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Entities.User;
@@ -17,5 +19,23 @@ public class UserService {
     public List<User> getUsers(){
 
         return _userRepository.findAll();
+    }
+
+    public ResponseEntity<String> createUser(User user){
+        User newUser = user;
+
+        if(getUser(newUser.getId()) == null){
+            return ResponseEntity.status(403).body("User already exist.");
+        }
+
+        _userRepository.save(newUser);
+        return ResponseEntity.ok().body("User created");
+
+    }
+
+    public Optional<User> getUser(long id){
+        Optional<User> user = _userRepository.findById(id);
+        
+        return user;
     }
 }
