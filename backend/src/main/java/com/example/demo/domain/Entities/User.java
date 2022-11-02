66b1f1350a229f233b3b896bc07787515ffb1480
kelvin-had1 <kelvinmendes900@gmail.com>
2022-11-02 17:43:59 -0300

    feat: refact ResponseMessage

--- backend/src/main/java/com/example/demo/domain/Entities/User.java
+++ backend/src/main/java/com/example/demo/domain/Entities/User.java
@@ -1,5 +1,10 @@
 package com.example.demo.domain.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +14,10 @@ import javax.persistence.Id;
 
 @Entity
 @Table(name = "User")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -23,50 +32,6 @@ public class User {
 
     @Column(name = "email")
     private String email;
-    
-    public User(){
 
-    }
-
-    public User(long id, String name, String password, String email) {
-        Id = id;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
-
-    public long getId() {
-        return Id;
-    }
-
-    public void setId(long id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    
 
 }
--- backend/src/main/java/com/example/demo/domain/Entities/UserLoginRequest.java
+++ backend/src/main/java/com/example/demo/domain/Entities/UserLoginRequest.java
@@ -1,24 +1,16 @@
 package com.example.demo.domain.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserLoginRequest {
     private String email;
     private String password;
-    
-    public UserLoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
